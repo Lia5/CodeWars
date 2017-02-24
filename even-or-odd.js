9808c837c0d1ff745e26a1661d544d1b7ba14d28
Lia5 <Lia_na5@mail.ru>
2017-02-24 15:51:41 +0200

    Lesson 3

---
+++ even-or-odd.js
@@ -0,0 +1,12 @@
+function even_or_odd(number) {
+  var result;
+  if (number % 2 == 0) {
+    result = 'Even';    
+  } else {
+     result = 'Odd';
+     }
+    return result;
+}
+function even_or_odd(number) {
+  return (number % 2 == 0) ? 'Even' : 'Odd';  
+}
